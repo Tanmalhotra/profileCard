b7fb1df48117577465b874cc5ea8e8a2ff8a769c
Tanisha Malhotra <37237963+Tanmalhotra@users.noreply.github.com>
2025-08-30 05:59:00 +0000

    Added description and image

--- profile-card/src/ProfileCard.js
+++ profile-card/src/ProfileCard.js
@@ -1,5 +1,6 @@
 import React from "react";
 import "./ProfileCard.css"; // we'll add styles here
+import profileImg from "./assets/cake.png";
 
 const ProfileCard = () => {
   return (
@@ -7,22 +8,26 @@ const ProfileCard = () => {
     <div className="main">
     <div className="profile-card">
       <img
-        src="https://via.placeholder.com/150"
+        src={profileImg}
         alt="Profile"
         className="profile-image"
       />
       <h2 className="profile-name">Tanisha Malhotra</h2>
       <p className="profile-title">Senior Software Developer</p>
-            <p className="profile-description">Description</p>
+            <p className="profile-description">Senior front-end developer with 6+ years of experience and expertise in React, JavaScript, HTML, and CSS.</p>
 
 
       <div className="social-links">
         <a href="#" target="_blank" rel="noopener noreferrer">LinkedIn</a>
-        <a href="#" target="_blank" rel="noopener noreferrer">GitHub</a>
-        <a href="#" target="_blank" rel="noopener noreferrer">Twitter</a>
+        <a href="#" target="_blank" rel="noopener noreferrer">GitHub</a> 
       </div>
 
-      <button className="cta-button">Contact Me</button>
+<a
+  href="mailto:tanisha@example.com"
+  className="cta-button"
+>
+  Contact Me
+</a>
     </div></div></>
   );
 };
